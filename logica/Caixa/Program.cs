63f5ed4a8d02e7c353b1e7fcded6ef4da8f87eca
henriquelandim <32872996+henriquelandim@users.noreply.github.com>
2018-04-17 14:07:03 -0300

    Add files via upload

---
+++ logica/Caixa/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace Caixa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string produto; 
+           double preco;
+           bool FormaDePagamento;
+           double resultado;
+           double precofinal;
+
+                    Console.WriteLine("Especifique o produto:");
+                    produto = Console.ReadLine();
+
+                    Console.WriteLine("atribua o preco do produto");
+                    preco = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("O Pagamento será em dinheiro?");
+                    FormaDePagamento = bool.Parse(Console.ReadLine());
+ 
+                        if(FormaDePagamento == true){
+                            resultado = preco * 10/100;
+                            precofinal = preco-resultado;                     
+                        }
+                        else{
+                            resultado = preco * 5/100;
+                            precofinal = preco+resultado;                          
+                        }
+
+                        StreamWriter arquivo = new StreamWriter("dados.txt");
+                        arquivo.WriteLine("Produto especificado: "+produto);
+                        arquivo.WriteLine("Preço a vista do produto: "+preco);
+                        arquivo.WriteLine("Valor de desconto ou acréscimo: "+resultado);
+                        arquivo.WriteLine("Preço final do Produto: "+precofinal);
+                        arquivo.Close();
+
+
+                        Console.WriteLine("Produto especificado: "+produto);
+                        Console.WriteLine("Preço a vista do produto: "+preco);
+                        Console.WriteLine("Valor de desconto ou acréscimo: "+resultado);
+                        Console.WriteLine("Preço final do Produto: "+precofinal);
+        }
+    }
+}
---
+++ logica/PrimeiroProjeto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            double n1 ;
+            double n2 ;
+            double soma, subtrair, multiplicar, dividir, resto;
+
+            n1=50;
+            n2=20;
+
+            soma = n1+n2; 
+            subtrair = n1-n2;
+            multiplicar = n1*n2;
+            dividir = n1/n2;
+            resto = n1%n2;   
+            Console.WriteLine(multiplicar);
+            Console.WriteLine(dividir);
+            Console.WriteLine(resto);
+            Console.WriteLine(subtrair);
+            Console.WriteLine(soma);
+        }
+    }
+}
